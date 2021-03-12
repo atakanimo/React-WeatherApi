00e92f68ee48224eaf3b75b7f399ebceecf711e3
atakanimo <70342818+atakanimo@users.noreply.github.com>
2021-03-12 12:34:35 +0300

    First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState, useEffect } from 'react';
+import { Card, Button } from 'react-bootstrap'
+import NextDay from './Components/NextDays/NextDay'
+import { render } from '@testing-library/react';
+import { Animated } from "react-animated-css";
+import { animateScroll } from 'react-scroll';
 
 function App() {
+
+  const key = "93c797befa5240568ad213645210903";
+  const [inputCity, setCity] = useState("");
+  const [weatherData, setWeather] = useState("");
+  let value = inputCity;
+
+  useEffect(() => {
+    getData();
+  }, [inputCity]);
+
+  function getData() {
+    fetch("http://api.weatherapi.com/v1/forecast.json?key=" + key + "&q=" + inputCity + "&days=3")
+      .then(response => response.json())
+      .then(data => {
+        setWeather(data);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="SearchForm">
+        <Animated animationIn="bounce" animationOut="fadeOut" animationInDelay="300"  isVisible={true}>
+          <input
+            placeholder="
+          Please Enter a City!"
+            className="search"
+            onChange={(event) => {
+              setCity(event.target.value);
+              value = event.target.value;
+              console.log(value);
+            }}
+          />
+        </Animated>
+        {weatherData.current && (
+          <>
+            {/* Today */}
+            <div className="content" >
+              <Animated animationIn="bounceInLeft" animationOut="fadeOut" animationInDelay="200" isVisible={true}>
+                <div className="Card">
+                  <div >
+                    <Card className="cards" style={{ width: '18rem' }}>
+                      <Card.Img variant="top" src={weatherData.current.condition.icon} alt={'asdsadas'} />
+                      <Card.Body>
+                        <hr />
+                        <Card.Title className="cardTitle">{weatherData.location.name} <br></br>  <span>{weatherData.current.condition.text}</span></Card.Title>
+                        <Card.Text className="cardText">
+                          <h2>{weatherData.current.temp_c}</h2>
+                          <hr />
+                          <p>Reel Feel: {weatherData.current.feelslike_c}</p>
+                        </Card.Text>
+                        <hr></hr>
+                        <Button className="cardButton" variant="dark">Show Air Quality</Button>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </div>
+                <Button className="Get3Days" variant="dark" onClick={() => {
+                  animateScroll.scrollToBottom();
+                  render(
+                    <NextDay
+                      data={weatherData}
+                      city={value}
+                    />
+                  )
+                }}>Click For 3 Days Forecast</Button>{' '}
+              </Animated>
+
+            </div>
+          </>
+        )
+        }
+
+      </form>
+
     </div>
   );
 }
---
+++ src/Components/NextDays/NextDay.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { Card } from 'react-bootstrap';
+import './NextDay.scss'
+import PhotoBulut from './../../Photos/Bulut.jpg';
+import { Animated } from "react-animated-css";
+import { animateScroll, Button } from 'react-scroll';
+
+
+export default function NextDay(props) {
+
+  const key = "93c797befa5240568ad213645210903";
+  const weatherData = props.data;
+  const weatherNow = weatherData.current.condition.text;
+  const formElement = document.querySelector(".SearchForm");
+
+  useEffect(() => {
+  
+  });
+
+  changePhoto();
+
+
+  function changePhoto() {
+    if (weatherNow.toLowerCase().includes("cloud")) {
+      console.log("Bulutlu")
+      formElement.style.backgroundColor = 'red';
+    } else if (weatherNow.toLowerCase().includes("sunny")) {
+      console.log("güneş");
+    } else if (weatherNow.toLowerCase().includes("snow")) {
+      console.log("kar");
+    } else if (weatherNow.toLowerCase().includes("clear")) {
+      console.log("hava açık");
+    } else if (weatherNow.toLowerCase().includes("rain")) {
+      console.log("yağmur")
+    } else {
+      console.log("no");
+    }
+  }
+
+  return (
+    <div id="NextDay" className="NextDay">
+      {/* 3DaysForecast */}
+      <div className="Next3Days">
+        <div className="Text">
+          <Animated animationIn="bounceInLeft" animationOut="fadeOut" animationInDelay="600" isVisible={true}>
+            <div>
+              <h1>Next 3 Days Forecast in {props.city}</h1>
+            </div>
+          </Animated>
+        </div>
+        <div className="showForecast">
+          <Card className="cards" style={{ width: '17rem', height: "26rem" }}>
+            <Card.Img variant="top" src={weatherData.forecast.forecastday[0].day.condition.icon} alt={'asdsadas'} />
+            <Card.Body>
+              <Card.Title>{weatherData.location.name} - <span>{weatherData.forecast.forecastday[0].day.condition.text}</span></Card.Title>
+              <Card.Text>
+                <p>{weatherData.forecast.forecastday[0].date}</p>
+                <p>Ave. Temp: <h2>{weatherData.forecast.forecastday[0].day.avgtemp_c}</h2></p>
+                <p>Sunrise:  {weatherData.forecast.forecastday[0].astro.sunrise}</p>
+                <p>Sunset:  {weatherData.forecast.forecastday[0].astro.sunset}</p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="cards" style={{ width: '17rem', height: "26rem" }}>
+            <Card.Img variant="top" src={weatherData.forecast.forecastday[1].day.condition.icon} alt={'asdsadas'} />
+            <Card.Body>
+              <Card.Title>{weatherData.location.name} - <span>{weatherData.forecast.forecastday[1].day.condition.text}</span></Card.Title>
+              <Card.Text>
+                <p>{weatherData.forecast.forecastday[1].date}</p>
+                <p>Ave. Temp: <h2>{weatherData.forecast.forecastday[1].day.avgtemp_c}</h2></p>
+                <p>Sunrise:  {weatherData.forecast.forecastday[1].astro.sunrise}</p>
+                <p>Sunset:  {weatherData.forecast.forecastday[1].astro.sunset}</p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="cards" style={{ width: '17rem', height: "26rem" }}>
+            <Card.Img variant="top" src={weatherData.forecast.forecastday[2].day.condition.icon} alt={'asdsadas'} />
+            <Card.Body>
+              <Card.Title>{weatherData.location.name} - <span>{weatherData.forecast.forecastday[2].day.condition.text}</span></Card.Title>
+              <Card.Text>
+                <p>{weatherData.forecast.forecastday[2].date}</p>
+                <p>Ave. Temp: <h2>{weatherData.forecast.forecastday[2].day.avgtemp_c}</h2></p>
+                <p>Sunrise:  {weatherData.forecast.forecastday[2].astro.sunrise}</p>
+                <p>Sunset:  {weatherData.forecast.forecastday[2].astro.sunset}</p>
+              </Card.Text>
+            </Card.Body>
+          </Card>         
+        </div>
+        <button onClick={() => animateScroll.scrollToTop()}>Back the Top!</button>
+      </div>
+    </div>
+  )
+}
