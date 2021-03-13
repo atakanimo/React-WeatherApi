9d3887eef0692496b8d90ab98296093834ace60b
atakanimo <70342818+atakanimo@users.noreply.github.com>
2021-03-14 00:52:48 +0300

    FullyResponsible

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import './AppMedia.scss';
 import { useState, useEffect } from 'react';
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button, Container, Row } from 'react-bootstrap'
 import NextDay from './Components/NextDays/NextDay'
 import { render } from '@testing-library/react';
 import { Animated } from "react-animated-css";
@@ -21,70 +21,72 @@ function App() {
 
   function getData() {
     axios.get("https://api.weatherapi.com/v1/forecast.json?key=" + key + "&q=" + inputCity + "&days=3")
-    .then(res => {
-      const data = res.data;
-      setWeather(data);
-      console.log(data);
-    })
+      .then(res => {
+        const data = res.data;
+        setWeather(data);
+        console.log(data);
+      })
   }
 
   return (
     <div className="App">
-      <form className="SearchForm">
-        <Animated animationIn="bounce" animationOut="fadeOut" animationInDelay="300"  isVisible={true}>
-          <input
-            placeholder="
+      <Container className="container-Form">
+        <form className="SearchForm">
+          <Animated animationIn="bounce" animationOut="fadeOut" animationInDelay="300" isVisible={true}>
+            <input
+              placeholder="
           Please Enter a City!"
-            className="search"
-            onChange={(event) => {
-              setCity(event.target.value);
-              value = event.target.value;
-              console.log(value);
-            }}
-          />
-        </Animated>
-        {weatherData.current && (
-          <>
-            {/* Today */}
-            <div className="content" >
-              <Animated animationIn="bounceInLeft" animationOut="fadeOut" animationInDelay="200" isVisible={true}>
-                <div className="Card">
-                  <div >
-                    <Card className="cards" >
-                      <Card.Img variant="top" src={weatherData.current.condition.icon} alt={'asdsadas'} />
-                      <Card.Body>
-                        <hr />
-                        <Card.Title className="cardTitle">{weatherData.location.name} <br></br>  <span>{weatherData.current.condition.text}</span></Card.Title>
-                        <Card.Text className="cardText">
-                          <h2>{weatherData.current.temp_c}°</h2>
+              className="search"
+              onChange={(event) => {
+                setCity(event.target.value);
+                value = event.target.value;
+                console.log(value);
+              }}
+            />
+          </Animated>
+        </form>
+      </Container>
+      <Container className="container-Card">
+        <Row className="Card-row">
+          {weatherData.current && (
+            <>
+              <div className="content" >
+                <Animated animationIn="bounceInLeft" animationOut="fadeOut" animationInDelay="200" isVisible={true}>
+
+                      <Card className="cards" >
+                        <Card.Img variant="top" src={weatherData.current.condition.icon} alt={'asdsadas'} />
+                        <Card.Body>
                           <hr />
-                          <p>Reel Feel: {weatherData.current.feelslike_c}</p>
-                        </Card.Text>
-                        <hr></hr>
-                        <Button className="cardButton" variant="dark">Show Air Quality</Button>
-                      </Card.Body>
-                    </Card>
-                  </div>
-                </div>
-                <Animated animationIn="bounce" animationOut="fadeOut" animationInDelay="800"  isVisible={true}>
-                <Button className="Get3Days" variant="dark" onClick={() => {
-                  animateScroll.scrollToBottom();
-                  render(
-                    <NextDay
-                      weatherData={weatherData}
-                      city={value}
-                    />
-                  )
-                }}>Click For 3 Days Forecast</Button>{' '}
+                          <Card.Title className="cardTitle">{weatherData.location.name} <br></br>  <span>{weatherData.current.condition.text}</span></Card.Title>
+                          <Card.Text className="cardText">
+                            <h2>{weatherData.current.temp_c}°</h2>
+                            <hr />
+                            <p>Reel Feel: {weatherData.current.feelslike_c}</p>
+                          </Card.Text>
+                          <hr></hr>
+                          <Button className="cardButton" variant="dark">Show Air Quality</Button>
+                        </Card.Body>
+                      </Card>
+
+                  <Animated animationIn="bounce" animationOut="fadeOut" animationInDelay="800" isVisible={true}>
+                    <Button className="Get3Days" variant="dark" onClick={() => {
+                      animateScroll.scrollToBottom();
+                      render(
+                        <NextDay
+                          weatherData={weatherData}
+                          city={value}
+                        />
+                      )
+                    }}>Click For 3 Days Forecast</Button>{' '}
+                  </Animated>
                 </Animated>
-              </Animated>
+              </div>
+            </>
+          )
+          }
+        </Row>
+      </Container>
 
-            </div>
-          </>
-        )
-        }
-
-      </form>
     </div>
   );
 }
